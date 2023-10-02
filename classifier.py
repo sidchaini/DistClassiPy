18bef1c82d8f109f3b326283fa97057c536eb833
Siddharth Chaini <40721514+sidchaini@users.noreply.github.com>
2023-10-02 17:30:54 -0400

    Added comments

--- classifier.py
+++ classifier.py
@@ -1,18 +1,17 @@
 import numpy as np
 import pandas as pd
 from scipy.spatial import distance
-import distances
-cd = distances.Distance()
+from distances import Distance
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.neighbors import KernelDensity
-from sklearn.utils._param_validation import InvalidParameterError
 
 class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
-    """ A distance metric classifier based on scikit-learn.
+    """
+    A distance metric classifier based on scikit-learn.
     
     Parameters
     ----------
@@ -25,29 +24,61 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
     
     """
     def __init__(self, metric="euclidean", scale_std=True, n_feat_select=None,
-                 canonical_stat="median",
-                 calculate_kde=True, calculate_1d_dist=True, # Maybe change to False
-                 n_jobs=-1):
+                 canonical_stat="median", calculate_kde=True, 
+                 calculate_1d_dist=True, n_jobs=-1):
+        """
+        Initialize the classifier with the given parameters.
+        
+        Parameters
+        ----------
+        metric : str or callable, optional
+            The distance metric to use.
+        scale_std : bool, optional
+            If True, classifier is scaled in terms of standard deviations.
+        n_feat_select : int, optional
+            The number of features to select.
+        canonical_stat : str, optional
+            The statistical measure to use for creating the training template.
+        calculate_kde : bool, optional
+            If True, calculate the kernel density estimate.
+        calculate_1d_dist : bool, optional
+            If True, calculate the 1-dimensional distance.
+        n_jobs : int, optional
+            The number of jobs to run in parallel.
+        """
         self.metric = metric
-        self.scale_std=scale_std
+        self.scale_std = scale_std
         self.canonical_stat = canonical_stat
         self.n_feat_select = n_feat_select
         self.calculate_kde = calculate_kde
         self.calculate_1d_dist = calculate_1d_dist
         self.n_jobs = n_jobs
+        self.distance_calculator = Distance()
 
     def fit(self, X, y, feat_labels=None):
+        """
+        Fit the classifier to the data.
+        
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            The training input samples.
+        y : array-like of shape (n_samples,)
+            The target values.
+        feat_labels : list of str, optional
+            The feature labels.
+        """
         X, y = check_X_y(X, y)
         if self.n_feat_select:
             feat_selector = SequentialFeatureSelector(
-                LCDistanceClassifier(
+                DistanceMetricClassifier(
                     metric=self.metric, scale_std=self.scale_std,
-                    n_feat_select=None, canonical_stat = self.canonical_stat
+                    n_feat_select=None, canonical_stat=self.canonical_stat
                     ),
                 n_features_to_select=self.n_feat_select, direction="forward", n_jobs=self.n_jobs
                 ).fit(X, y)
             self.keep_indices = feat_selector.get_support()
-            X = X[:,self.keep_indices]
+            X = X[:, self.keep_indices]
 
         self.classes_ = unique_labels(y)
         self.n_features_in_ = X.shape[1]
@@ -57,11 +88,11 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
         
         canonical_list = []
         for cur_class in self.classes_:
-            cur_X = X[np.argwhere(y==cur_class)]
+            cur_X = X[np.argwhere(y == cur_class)]
             if self.canonical_stat == "median":
-                canonical_list.append(np.median(cur_X,axis=0).ravel())
+                canonical_list.append(np.median(cur_X, axis=0).ravel())
             elif self.canonical_stat == "mean":
-                canonical_list.append(np.mean(cur_X,axis=0).ravel())
+                canonical_list.append(np.mean(cur_X, axis=0).ravel())
         df_canonical = pd.DataFrame(
             data=np.array(canonical_list),
             index=self.classes_,
@@ -71,9 +102,9 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
         if self.scale_std:
             std_list = []
             for cur_class in self.classes_:
-                cur_X = X[y==cur_class]
+                cur_X = X[y == cur_class]
                 # Note we're using ddof=1 because we're dealing with a sample. See more: https://stackoverflow.com/a/46083501/10743245
-                std_list.append(np.std(cur_X,axis=0,ddof=1).ravel()) 
+                std_list.append(np.std(cur_X, axis=0, ddof=1).ravel()) 
             df_std = pd.DataFrame(
                         data=np.array(std_list),
                         index=self.classes_,
@@ -86,11 +117,11 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
             
             
             for cl in self.classes_:
-                subX = X[y==cl]
+                subX = X[y == cl]
                 # Implement the following in an if-else to save computational time.
                 # kde = KernelDensity(bandwidth='scott', metric=self.metric)
                 # kde.fit(subX)
-                kde = KernelDensity(bandwidth='scott', metric='pyfunc',metric_params={"func":self.metric_fn_})
+                kde = KernelDensity(bandwidth='scott', metric='pyfunc', metric_params={"func": self.metric_fn_})
                 kde.fit(subX)
                 self.kde_dict_[cl] = kde
 
@@ -99,11 +130,19 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
         return self
 
     def predict(self, X):
+        """
+        Predict the class labels for the provided data.
+        
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            The input samples.
+        """
         check_is_fitted(self, 'is_fitted_')
         X = check_array(X)
         
         if self.n_feat_select:
-            X = X[:,self.keep_indices]
+            X = X[:, self.keep_indices]
 
         if not self.scale_std:
             dist_arr = distance.cdist(
@@ -113,15 +152,15 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
 
         else:
             dist_arr_list = []
-            wtdf = 1/self.df_std_
+            wtdf = 1 / self.df_std_
             wtdf = wtdf.replace([np.inf, -np.inf], np.nan)
             wtdf = wtdf.fillna(0)
 
             for cl in self.classes_:
-                XB = self.df_canonical_.loc[cl].to_numpy().reshape(1,-1)
-                w = wtdf.loc[cl].to_numpy() # 1/std dev
-                XB = XB*w # w is for this class only
-                XA = X*w # w is for this class only
+                XB = self.df_canonical_.loc[cl].to_numpy().reshape(1, -1)
+                w = wtdf.loc[cl].to_numpy()  # 1/std dev
+                XB = XB * w  # w is for this class only
+                XA = X * w  # w is for this class only
                 cl_dist = distance.cdist(XA=XA,
                                          XB=XB,
                                          metric=self.metric)
@@ -132,14 +171,16 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
         return y_pred
     
     def set_metric_fn(self):
-        # Set metric fn
+        """
+        Set the metric function.
+        """
         if not callable(self.metric) or isinstance(self.metric, str):
             if hasattr(distance, self.metric):
                 self.metric_fn_ = getattr(distance, self.metric)
-            elif hasattr(cd, self.metric):    
-                self.metric_fn_ = getattr(cd, self.metric)
+            elif hasattr(self.distance_calculator, self.metric):    
+                self.metric_fn_ = getattr(self.distance_calculator, self.metric)
             else:
-                raise ValueError(f"{metric} metric not found. Either pass a string of the name of a metric in scipy.spatial.distance or distances.Distance, or, pass a metric function directly.")
+                raise ValueError(f"{self.metric} metric not found. Either pass a string of the name of a metric in scipy.spatial.distance or distances.Distance, or, pass a metric function directly.")
 
         else:
             self.metric_fn_ = self.metric
@@ -147,11 +188,19 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
         
 
     def predict_and_analyse(self, X):
+        """
+        Predict the class labels for the provided data and perform analysis.
+        
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            The input samples.
+        """
         check_is_fitted(self, 'is_fitted_')
         X = check_array(X)
         
         if self.n_feat_select:
-            X = X[:,self.keep_indices]
+            X = X[:, self.keep_indices]
 
         if not self.scale_std:
             dist_arr = distance.cdist(
@@ -161,16 +210,16 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
 
         else:
             dist_arr_list = []
-            wtdf = 1/self.df_std_
+            wtdf = 1 / self.df_std_
             wtdf = wtdf.replace([np.inf, -np.inf], np.nan)
             wtdf = wtdf.fillna(0)
             self.wtdf_ = wtdf
 
             for cl in self.classes_:
-                XB = self.df_canonical_.loc[cl].to_numpy().reshape(1,-1)
-                w = wtdf.loc[cl].to_numpy() # 1/std dev
-                XB = XB*w # w is for this class only
-                XA = X*w # w is for this class only
+                XB = self.df_canonical_.loc[cl].to_numpy().reshape(1, -1)
+                w = wtdf.loc[cl].to_numpy()  # 1/std dev
+                XB = XB * w  # w is for this class only
+                XA = X * w  # w is for this class only
                 cl_dist = distance.cdist(XA=XA,
                                          XB=XB,
                                          metric=self.metric)
@@ -186,7 +235,7 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
                 
         if self.calculate_kde:
             # NEW: Rescale in terms of median likelihoods - calculate here
-            scale_factors = np.exp([self.kde_dict_[cl].score_samples(self.df_canonical_.loc[cl].to_numpy().reshape(1,-1))[0] for cl in self.classes_])
+            scale_factors = np.exp([self.kde_dict_[cl].score_samples(self.df_canonical_.loc[cl].to_numpy().reshape(1, -1))[0] for cl in self.classes_])
                         
             likelihood_arr = []
             for k in self.kde_dict_.keys():
@@ -196,7 +245,7 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
             self.likelihood_arr_ = np.array(likelihood_arr).T
             
             # NEW: Rescale in terms of median likelihoods - rescale here
-            self.likelihood_arr_ = self.likelihood_arr_/scale_factors
+            self.likelihood_arr_ = self.likelihood_arr_ / scale_factors
 
         if self.calculate_1d_dist:
             conf_cl = []
@@ -205,11 +254,11 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
                 sum_1d_dists = np.zeros(shape=(len(Xdf_temp)))
                 for feat in Xdf_temp.columns:
                     dists = distance.cdist(
-                    XA=np.zeros(shape=(1,1)),
-                    XB=(self.df_canonical_.loc[cl]-Xdf_temp)[feat].to_numpy().reshape(-1,1), 
+                    XA=np.zeros(shape=(1, 1)),
+                    XB=(self.df_canonical_.loc[cl] - Xdf_temp)[feat].to_numpy().reshape(-1, 1), 
                     metric=self.metric).ravel()
-                    sum_1d_dists = sum_1d_dists + dists/self.df_std_.loc[cl,feat]
-                confs = 1/sum_1d_dists
+                    sum_1d_dists = sum_1d_dists + dists / self.df_std_.loc[cl, feat]
+                confs = 1 / sum_1d_dists
                 conf_cl.append(confs)
             conf_cl = np.array(conf_cl)
             self.conf_cl_ = conf_cl
@@ -221,23 +270,31 @@ class DistanceMetricClassifier(BaseEstimator, ClassifierMixin):
 
     
     def calculate_confidence(self, method="distance_inverse"):
+        """
+        Calculate the confidence for each prediction.
+        
+        Parameters
+        ----------
+        method : str, optional
+            The method to use for calculating confidence.
+        """
         check_is_fitted(self, 'is_fitted_')
         if not hasattr(self, 'analyis_'):
             raise ValueError("Use predict_and_analyse() instead of predict() for confidence calculation.")
         
         # Calculate confidence for each prediction
-        if method=="distance_inverse":
-            self.confidence_df_ = 1/self.canonical_dist_df_
-            self.confidence_df_.columns = [x.replace("_dist","_conf") for x in self.confidence_df_.columns]
+        if method == "distance_inverse":
+            self.confidence_df_ = 1 / self.canonical_dist_df_
+            self.confidence_df_.columns = [x.replace("_dist", "_conf") for x in self.confidence_df_.columns]
             
-        elif method=="1d_distance_inverse":
+        elif method == "1d_distance_inverse":
             if not self.calculate_1d_dist:
                 raise ValueError("method='1d_distance_inverse' is only valid if calculate_1d_dist is set to True")
             self.confidence_df_ = pd.DataFrame(
                 data=self.conf_cl_.T, 
                 columns=[f"{x}_conf" for x in self.classes_])
                 
-        elif method=="kde_likelihood":
+        elif method == "kde_likelihood":
             if not self.calculate_kde:
                 raise ValueError("method='kde_likelihood' is only valid if calculate_kde is set to True")
         
