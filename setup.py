921e25dfeb77b93096864f25c565472b866e0ddb
Siddharth Chaini <40721514+sidchaini@users.noreply.github.com>
2023-10-23 23:13:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="distclassipy",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
     author="Siddharth Chaini",
     author_email="sidchaini@gmail.com",
