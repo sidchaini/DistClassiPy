2e6d0ed8222a476ca2bd713dc42510d91ee41d4e
Siddharth Chaini <40721514+sidchaini@users.noreply.github.com>
2024-02-07 19:28:12 -0500

    Removed extra files from pypi

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "DistClassiPy"
 copyright = "2024, Siddharth Chaini"
 author = "Siddharth Chaini"
-release = "0.0.7"
+release = "0.0.8"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
