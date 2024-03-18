8e1bd755e0ac345eeb20f04acfba98e925269d7f
Siddharth Chaini <40721514+sidchaini@users.noreply.github.com>
2024-03-18 14:02:50 +0530

    Added tests for all distances

--- tests/test_distances.py
+++ tests/test_distances.py
@@ -6,30 +6,12 @@ from distclassipy.distances import Distance
 distance = Distance()
 
 
-# Test for the accuracy distance calculation
-def test_acc_distance():
+def test_all_distances():
     # Define two sample vectors
     u = np.array([1, 2, 3])
-    v = np.array([2, 4, 6])
-    # Calculate the expected result manually
-    expected = np.mean([np.sum(np.abs(u - v)), np.max(np.abs(u - v))])
-    # Assert that the calculated distance matches the expected result
-    assert distance.acc(u, v) == expected
-
-
-# Test for the Vicis Wave Hedges distance calculation
-def test_vicis_wave_hedges():
-    # Define two sample vectors
-    u = np.array([1, 2, 3])
-    v = np.array([2, 4, 6])
-    # Calculate the minimum of u and v element-wise
-    uvmin = np.minimum(u, v)
-    # Calculate the absolute difference between u and v
-    u_v = np.abs(u - v)
-    # Calculate the expected result manually
-    expected = np.sum(np.where(uvmin != 0, u_v / uvmin, 0))
-    # Assert that the calculated distance matches the expected result
-    assert distance.vicis_wave_hedges(u, v) == expected
-
-
-# HAVE TO ADD MORE TESTS
+    v = np.array([1, 2, 3])
+    for func_name in dir(distance):
+        if callable(getattr(distance, func_name)) and not func_name.startswith("__"):
+            func = getattr(distance, func_name)
+            d = func(u, v)
+            assert d >= 0
