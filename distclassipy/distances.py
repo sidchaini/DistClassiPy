01b51fca5f0e6f43299d8157a33c417940a23a94
Siddharth Chaini <40721514+sidchaini@users.noreply.github.com>
2024-05-12 16:12:32 -0400

    Added license statement to each source file

--- distclassipy/distances.py
+++ distclassipy/distances.py
@@ -25,6 +25,20 @@ Notes
     In addition, the following code was added to all functions for array conversion:
         u,v = np.asarray(u), np.asarray(v)
 -----
+Copyright (C) 2024  Siddharth Chaini
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import numpy as np
