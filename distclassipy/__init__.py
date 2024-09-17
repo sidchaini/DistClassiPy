f895f5b597315a8bc7b4aef9f711b4d2f1338ece
Siddharth Chaini <40721514+sidchaini@users.noreply.github.com>
2024-09-17 11:53:27 +0100

    Bump version to 0.1.6a0

--- distclassipy/__init__.py
+++ distclassipy/__init__.py
@@ -25,4 +25,4 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from .classifier import DistanceMetricClassifier  # noqa
 from .distances import Distance  # noqa
 
-__version__ = "0.1.5"
+__version__ = "0.1.6a0"
